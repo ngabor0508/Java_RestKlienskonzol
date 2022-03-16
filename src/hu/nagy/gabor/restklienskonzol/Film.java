c1ae04cd69216c3eb37c8b070ed54d03f3e69451
Nagy Gabor <ngabor0508@gmail.com>
2022-03-16 09:44:15 +0100

    Film osztály létrehozva, kiiratás, filmhozzáadás kész.

---
+++ src/hu/nagy/gabor/restklienskonzol/Film.java
@@ -0,0 +1,59 @@
+package hu.nagy.gabor.restklienskonzol;
+
+public class Film {
+    private int id;
+    private String cim;
+    private String kategoria;
+    private int hossz;
+    private int ertekeles;
+
+    public Film(int id, String cim, String kategoria, int hossz, int ertekeles) {
+        this.id = id;
+        this.cim = cim;
+        this.kategoria = kategoria;
+        this.hossz = hossz;
+        this.ertekeles = ertekeles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCim() {
+        return cim;
+    }
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public int getHossz() {
+        return hossz;
+    }
+
+    public int getErtekeles() {
+        return ertekeles;
+    }
+
+    public void setCim(String cim) {
+        this.cim = cim;
+    }
+
+    public void setKategoria(String kategoria) {
+        this.kategoria = kategoria;
+    }
+
+    public void setHossz(int hossz) {
+        this.hossz = hossz;
+    }
+
+    public void setErtekeles(int ertekeles) {
+        this.ertekeles = ertekeles;
+    }
+
+    public String toJson(){
+        return String.format("{\"cim\":\"%s\",\"kategoria\":\"%s\",\"hossz\":%d,\"ertekeles\":%d}",
+                this.cim, this.kategoria, this.hossz, this.ertekeles
+        );
+    }
+}
--- src/hu/nagy/gabor/restklienskonzol/Main.java
+++ src/hu/nagy/gabor/restklienskonzol/Main.java
@@ -1,17 +1,15 @@
 package hu.nagy.gabor.restklienskonzol;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
     public static void main(String[] args) {
-        String url = "";
+        String url = "https://retoolapi.dev/Q304E6/film";
         try {
             osszesFilmkiirasa(url);
         } catch (IOException e) {
@@ -22,7 +20,27 @@ public class Main {
     private static void osszesFilmkiirasa(String url) throws IOException {
         URL urlObject = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) urlObject.openConnection();
-        conn.setRequestProperty("Accept", "text/csv");
+        conn.setRequestProperty("Accept", "text/json");
+        InputStream is = conn.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String sor = br.readLine();
+        while (sor != null){
+            System.out.println(sor);
+            sor = br.readLine();
+        }
+        br.close();
+        is.close();
+    }
+
+    public static void filmHozzaadasa(String url) throws IOException{
+        URL urlObject = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) urlObject.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Accept", "application/json");
+        OutputStream os = conn.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8));
+
         InputStream is = conn.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         String sor = br.readLine();
