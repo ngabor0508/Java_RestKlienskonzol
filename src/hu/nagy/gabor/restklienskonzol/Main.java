1b682241749e44f5f5177f09b41f2c0d11369ae3
ngabor0508 <ngabor0508@gmail.com>
2022-03-16 11:50:31 +0100

    Kiíratás, beszúrás kész.

--- src/hu/nagy/gabor/restklienskonzol/Main.java
+++ src/hu/nagy/gabor/restklienskonzol/Main.java
@@ -11,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         String url = "https://retoolapi.dev/Q304E6/film";
         try {
+            Film film = new Film(0, "Teszt", "kaland", 120, 7);
+            filmHozzaadasa(url, film);
             osszesFilmkiirasa(url);
         } catch (IOException e) {
             System.out.println(e);
@@ -32,14 +34,19 @@ public class Main {
         is.close();
     }
 
-    public static void filmHozzaadasa(String url) throws IOException{
+    public static void filmHozzaadasa(String url, Film film) throws IOException{
         URL urlObject = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) urlObject.openConnection();
+        conn.setDoOutput(true);
         conn.setRequestMethod("POST");
         conn.setRequestProperty("Content-Type", "application/json");
         conn.setRequestProperty("Accept", "application/json");
         OutputStream os = conn.getOutputStream();
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8));
+        writer.write(film.toJson());
+        writer.flush();
+        writer.close();
+        os.close();
 
         InputStream is = conn.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
